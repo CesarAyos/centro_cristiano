e72c5d379f942725f7c93e05f1290e06d9a5416f
CesarAyos <ayos007@gmail.com>
2024-01-30 00:16:47 -0400

    conectar

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
   database: {
     host: "dpg-cms6tiicn0vc73becmhg-a",
     user: "centro",
-    password: "",
+    password: "PW20xnB5uiCAzpSDRsHPhASL2DRkgEw5",
     database: "centro",
   },
 };
