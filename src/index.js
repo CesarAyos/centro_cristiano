897a6db0616da84ac1b9c6d568350a1309394d69
CesarAyos <ayos007@gmail.com>
2024-01-30 11:13:34 -0400

    conection

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan");
 const mysql = require("mysql");
 const multer = require("multer");
 const bodyParser = require("body-parser");
-const {Pool} =  require ('pg');
+
 
 
 //plantilla,
@@ -80,6 +80,31 @@ app.listen(app.get("port"), (err, res) => {
 // Establecer el motor de plantillas
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
+
+
+
+
+
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'dpg-cms6tiicn0vc73becmhg-a',
+  port: 5432,
+  database: 'centro',
+  user: 'centro',
+  password: 'PW20xnB5uiCAzpSDRsHPhASL2DRkgEw5', 
+});
+
+client.connect(err => {
+  if (err) {
+    console.error('Error de conexión', err.stack);
+  } else {
+    console.log('Conectado a PostgreSQL');
+  }
+});
+
 
 
 
--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
-module.exports = {
-  database: {
-    host: "dpg-cms6tiicn0vc73becmhg-a",
-    Port: "5432",
-    user: "centro",
-    password: "PW20xnB5uiCAzpSDRsHPhASL2DRkgEw5",
-    database: "centro",
-  },
-};
+// module.exports = {
+//   database: {
+//     host: "dpg-cms6tiicn0vc73becmhg-a",
+//     Port: "5432",
+//     user: "centro",
+//     password: "PW20xnB5uiCAzpSDRsHPhASL2DRkgEw5",
+//     database: "centro",
+//   },
+// };
