10d09ea81863cd5b56321a442a7ded7a0e060982
CesarAyos <ayos007@gmail.com>
2024-01-30 12:27:12 -0400

    conection

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,9 @@
-// module.exports = {
-//   database: {
-//     host: "dpg-cms6tiicn0vc73becmhg-a",
-//     Port: "5432",
-//     user: "centro",
-//     password: "PW20xnB5uiCAzpSDRsHPhASL2DRkgEw5",
-//     database: "centro",
-//   },
-// };
-
-
+module.exports = {
+  database: {
+    host: "dpg-cms6tiicn0vc73becmhg-a",
+    port: "5432",
+    user: "centro",
+    password: "PW20xnB5uiCAzpSDRsHPhASL2DRkgEw5",
+    database: "centro?sslmode=no-verify",
+  },
+};
